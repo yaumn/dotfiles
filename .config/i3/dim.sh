c0fb782b63579fa58dcd9b95bb13130c5c83b4e9
yaumn <guillaume.andre33@gmail.com>
2021-03-28 22:31:27 +0200

    Update files

--- .config/i3/dim.sh
+++ .config/i3/dim.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 
-trap 'brightnessctl s 100% ; kill %%' EXIT
-brightnessctl s 50%
-sleep 2147483647 &
+BRIGHTNESS=$(brightnessctl g)
+NEW_BRIGHTNESS=$((BRIGHTNESS / 2))
+
+trap 'kill $! ; exit 0' HUP
+trap "brightnessctl s $BRIGHTNESS" EXIT
+brightnessctl s $NEW_BRIGHTNESS
+sleep infinity &
 wait
--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-#set -e
-#xset s off dpms 0 10 0
-systemctl suspend
-#xset s off -dpms
+/usr/bin/i3lock --color=000000 --ignore-empty-password --nofork
